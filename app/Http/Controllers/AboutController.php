2c752089ce7b211aa6fea1323d59008d00902a5a
Aphisit Sangsiwi <nhcomsci61@gmail.com>
2024-05-29 15:07:40 +0700

    Get value from Controller to View

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -7,10 +7,9 @@ use Illuminate\Http\Request;
 class AboutController extends Controller
 {
     function index(){
-        return view("about");
-    }
-
-    function showData(){
-        echo "Hello Laravel";
+        $address = "4 กรุงเทพมหานคร ประเทศไทย";
+        $tel = "0928885608";
+        $email = "nhcomsci61@gmail.com";
+        return view("about",["address"=>$address, 'tel'=>$tel, 'email'=>$email]);
     }
 }   
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,6 +10,9 @@
 
 <body>
     <h2>เกี่ยวกับเรา</h2>
+    <h3>{{$address}}</h3>
+    <h3>{{$tel}}</h3>
+    <h3>{{$email}}</h3>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum dolores quidem quam, magni, reiciendis in ab
         praesentium sequi ut recusandae minima sit accusantium rerum dicta at cumque expedita aliquid dolorum?</p>
         <a href="{{url('/')}}">home</a>
